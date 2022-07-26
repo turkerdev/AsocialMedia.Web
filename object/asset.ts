5dd268f03c30939c9f1694734ca1e85ead3e0467
Serhat TURKER <oqzcmcuxr@mozmail.com>
2022-07-26 20:42:52 +0300

    Change duration to end_time

--- object/asset.ts
+++ object/asset.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export const asset = z.object({
   url: z.string(),
   credit: z.string().optional(),
-  duration: z.string().optional(),
   start_time: z.string().optional(),
+  end_time: z.string().optional(),
 });
 
 export type Asset = z.infer<typeof asset>;
