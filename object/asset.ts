a907eb741e7faa21dbbf249ffb465c5b891546c1
Serhat TURKER <oqzcmcuxr@mozmail.com>
2022-07-26 13:37:07 +0300

    Add asset trimming

--- object/asset.ts
+++ object/asset.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 export const asset = z.object({
   url: z.string(),
   credit: z.string().optional(),
+  duration: z.string().optional(),
+  start_time: z.string().optional(),
 });
 
 export type Asset = z.infer<typeof asset>;
