92ed355968aa656524d542094264f23ea0f31e40
Serhat TURKER <oqzcmcuxr@mozmail.com>
2022-07-28 06:38:04 +0300

    Using mongodb now

--- services/db.ts
+++ services/db.ts
@@ -1,23 +1,23 @@
 import { prisma } from "../utils/prisma";
 
 export class DbService {
-  static async findYoutubeChannelById(id: string) {
+  static async findYoutubeChannelById(platform_id: string) {
     const account = await prisma.account.findUniqueOrThrow({
-      where: { platform_id: { platform: "YouTube", id } },
+      where: { platform_platform_id: { platform: "YouTube", platform_id } },
     });
 
     return account;
   }
 
   static async upsertYoutubeChannel(
-    id: string,
+    platform_id: string,
     access_token: string,
     refresh_token: string
   ) {
     await prisma.account.upsert({
-      where: { platform_id: { platform: "YouTube", id } },
+      where: { platform_platform_id: { platform: "YouTube", platform_id } },
       update: { access_token, refresh_token },
-      create: { platform: "YouTube", id, access_token, refresh_token },
+      create: { platform: "YouTube", platform_id, access_token, refresh_token },
     });
   }
 }
