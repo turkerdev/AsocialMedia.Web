191a384b9eac24e9b8cf10dc4f7c9451585638ae
Serhat TURKER <oqzcmcuxr@mozmail.com>
2022-08-05 23:43:24 +0300

    Add category support

--- object/queue/platform/youtube.ts
+++ object/queue/platform/youtube.ts
@@ -23,6 +23,44 @@ export const YouTube = z
       .date()
       .transform((date) => date.toISOString())
       .optional(),
+    category: z
+      .enum([
+        "animals",
+        "cars",
+        "comedy",
+        "education",
+        "entertainment",
+        "film",
+        "gaming",
+        "howto",
+        "music",
+        "people",
+        "sport",
+        "travel",
+        "news",
+        "tech",
+      ])
+      .default("entertainment")
+      .transform(async (val) => {
+        const records: Record<typeof val, [string, string]> = {
+          animals: ["Pets & Animals", "15"],
+          cars: ["Autos & Vehicles", "2"],
+          comedy: ["Comedy", "23"],
+          education: ["Education", "27"],
+          entertainment: ["Entertainment", "24"],
+          film: ["Film & Animation", "1"],
+          gaming: ["Gaming", "20"],
+          howto: ["Howto & Style", "26"],
+          music: ["Music", "10"],
+          people: ["People & Blogs", "22"],
+          sport: ["Sports", "17"],
+          travel: ["Travel & Events", "19"],
+          news: ["News & Politics", "25"],
+          tech: ["Science & Technology", "28"],
+        };
+
+        return records[val].at(1);
+      }),
   })
   .transform((val) => {
     return {
